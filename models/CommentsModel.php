ed113640b9dc67055dc105f1cdca2c748f1c006a
Thirion Lucas <lucasthirion@live.fr>
2023-02-02 22:06:32 +0100

    Comment Model

--- models/CommentsModel.php
+++ models/CommentsModel.php
@@ -2,6 +2,7 @@
 
 namespace models;
 
+use DateTime;
 use models\base\SQL;
 
 class CommentsModel extends SQL
@@ -15,15 +16,22 @@ class CommentsModel extends SQL
    * Méthode d'exemple permettant l'accès aux données avec une
    * requête préparée.
    */
-  public function getByMovie(int $id): \stdClass | null
+  public function getByMovie(int $id): array | null
   {
     $stmt = $this->getPdo()->prepare("SELECT * FROM comments WHERE movie_id=:id");
     $stmt->execute(["id" => $id]);
-    $res = $stmt->fetch(\PDO::FETCH_OBJ);
-    if ($res) return $res;
-    return null;
-    // $stmt = $this->getPdo()->prepare("SELECT * FROM comments WHERE movie_id=:id");
-    // $stmt->execute(["id" => $id]);
-    // return $stmt->fetch(\PDO::FETCH_OBJ);
+    return $stmt->fetchAll(\PDO::FETCH_OBJ);
+  }
+
+  public function addComment(int $id, string $comment, string $name, string $date): void
+  {
+    $stmt = $this->getPdo()->prepare("INSERT INTO comments (movie_id, content, name, date) VALUES (:id, :comment, :name, :date)");
+    $stmt->execute(["id" => $id, "comment" => $comment, "name" => $name, "date" => $date]);
+  }
+
+  public function deleteComment(int $id): void
+  {
+    $stmt = $this->getPdo()->prepare("DELETE FROM comments WHERE id=:id");
+    $stmt->execute(["id" => $id]);
   }
 }
